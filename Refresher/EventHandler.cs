23470bb314026811550f5c7e43dd45a1245c4733
DrVulpinus <siganus.vulpinus@live.com>
2015-06-06 11:10:13 -0400

    Added class to handle creating and execution of events

---
+++ Refresher/EventHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refresher
+{
+    class EventHandler
+    {
+        private List<EventTimer> events = new List<EventTimer>();
+        private WindowSwitcher ws = new WindowSwitcher();
+        public void addNewEvent(KeysEvent newEvent)
+        {
+            EventTimer newTimer = new EventTimer(newEvent);
+            newTimer.Elapsed += new System.Timers.ElapsedEventHandler(eventElapsed);
+            newTimer.Stop();
+        }
+        public void removeEvent(KeysEvent removeEvent)
+        {
+            
+            foreach (EventTimer eventTimer in events)
+            {
+                if (eventTimer.timedEvent == removeEvent)
+                {
+                    events.Remove(eventTimer);
+                    break;
+                }
+            }
+           
+        }
+
+        private void eventElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            EventTimer elapsedTimer = (EventTimer)sender;
+            KeysEvent thisEvent = elapsedTimer.timedEvent;
+            
+        }
+
+
+    }
+}
