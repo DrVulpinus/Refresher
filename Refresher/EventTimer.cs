83683b23394bd2534824dec195a1258247308415
DrVulpinus <siganus.vulpinus@live.com>
2015-06-06 11:09:41 -0400

    Added class to handle timing of events

---
+++ Refresher/EventTimer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Refresher
+{
+    public partial class EventTimer : Timer
+    {
+        public KeysEvent timedEvent { get; set; }
+        public EventTimer(KeysEvent _timedEvent)
+        {
+            timedEvent = _timedEvent;
+            Interval = timedEvent.interval;
+            AutoReset = true;
+            
+        }
+        
+    }
+}
